5455b810f11861bd45cdd402e839013822d79aa8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-09-15 18:05:25 -0300

    ajusta ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -12,23 +12,32 @@
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	size_t	s_len;
-	char	*substr;
+static size_t   ft_substr_len(char const *s, unsigned int start, size_t size)
+{       
+	size_t  s_len;
+	size_t  substr_len;
 
+	substr_len = 0;
 	s_len = ft_strlen(s);
-	if (len > s_len)
-		len = s_len;
-	substr = (char *)ft_calloc(len + 1, sizeof(char));
+	if (start >= s_len)
+			return (substr_len);
+	while (substr_len < size && s[substr_len])
+			substr_len++;
+	return (substr_len);
+}
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t  i;
+	char    *substr;
+	size_t  substr_len;
+	
+	substr_len = ft_substr_len(s, start, len);
+	substr = (char *)ft_calloc(substr_len + 1, sizeof(char));
 	if (substr == NULL)
-		return (NULL);
-	if (start > s_len)
-		return (substr);
+			return (NULL);
 	i = 0;
-	while (s[start] && i < len)
-		substr[i++] = s[start++];
-	substr[i] = 0;
+	while (s[start] && i < substr_len)
+			substr[i++] = s[start++];
 	return (substr);
 }

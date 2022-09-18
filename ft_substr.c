0694a86452aff0d1be21b80aefdd7f35e3e2239f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-09-18 13:21:19 -0300

    ajusta norminette substr

--- ft_substr.c
+++ ft_substr.c
@@ -12,32 +12,32 @@
 
 #include "libft.h"
 
-static size_t   ft_substr_len(char const *s, unsigned int start, size_t size)
-{       
-	size_t  s_len;
-	size_t  substr_len;
+static size_t	ft_substr_len(char const *s, unsigned int start, size_t size)
+{
+	size_t	s_len;
+	size_t	substr_len;
 
 	substr_len = 0;
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-			return (substr_len);
+		return (substr_len);
 	while (substr_len < size && s[substr_len + start])
-			substr_len++;
+		substr_len++;
 	return (substr_len);
 }
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t  i;
-	char    *substr;
-	size_t  substr_len;
-	
+	size_t	i;
+	char	*substr;
+	size_t	substr_len;
+
 	substr_len = ft_substr_len(s, start, len);
 	substr = (char *)ft_calloc(substr_len + 1, sizeof(char));
 	if (substr == NULL)
-			return (NULL);
+		return (NULL);
 	i = 0;
 	while (s[start] && i < substr_len)
-			substr[i++] = s[start++];
+		substr[i++] = s[start++];
 	return (substr);
 }

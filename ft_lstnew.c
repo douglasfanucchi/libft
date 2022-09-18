f66bcc3b03939d88d2353fd63aa1d43f3ea52948
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-09-18 18:28:57 -0300

    ajusta lstnew norminette

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -17,8 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*node;
 
 	node = (t_list *)malloc(sizeof(t_list));
-		if (node == NULL)
-			return (NULL);
+	if (node == NULL)
+		return (NULL);
 	node->next = NULL;
 	node->content = content;
 	return (node);

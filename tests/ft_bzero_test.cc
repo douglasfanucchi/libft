3d9ed084806f03743c22f501996cc4b116512cfd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-23 13:10:39 -0300

    tests: it checks ft_bzero to erase n bytes

--- tests/ft_bzero_test.cc
+++ tests/ft_bzero_test.cc
@@ -2,6 +2,7 @@
 
 extern "C" {
 	#include <libft.h>
+	#include <string.h>
 }
 
 TEST(FTbzero, itShouldEraseFirstByteFromInput) {
@@ -20,4 +21,14 @@ TEST(FTbzero, itShouldNotEraseAnyBytes) {
 	ft_bzero(buffer, 0);
 
 	ASSERT_STREQ(buffer, "hello world");
+}
+
+TEST(FTbzero, itShouldEraseNBytesStarting) {
+	size_t n = 5;
+	char buffer[12];
+	strcpy(buffer, "hello world");
+
+	ft_bzero(buffer, n);
+
+	ASSERT_TRUE(memcmp(buffer, "\0\0\0\0\0 world", 12) == 0);
 }

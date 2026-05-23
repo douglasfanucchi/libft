9380f1224646e677c3ee8b3eb47fd402bfe618f3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-23 14:11:32 -0300

    feat: it implements ft_memcpy

--- libft.h
+++ libft.h
@@ -11,5 +11,6 @@ int	ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *ptr, int c, size_t n);
 void	ft_bzero(void *ptr, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif

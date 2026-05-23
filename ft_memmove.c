648aea9eea641f686aff6dd3b85c16a337029664
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-23 15:10:33 -0300

    refact: removes unneeded brackets in ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,9 +2,8 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	if (src > dest) {
+	if (src > dest)
 		return (ft_memcpy(dest, src, n));
-	}
 	while (n > 0)
 	{
 		((char *)dest)[n - 1] = ((char *)src)[n - 1];

039ffb9281564a9ce7fa9fd626b0f40496c18dd8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-25 18:35:45 -0300

    tests: ft_strnstr should return first occurrence looking up to N bytes on haystack

--- tests/ft_strnstr_test.cc
+++ tests/ft_strnstr_test.cc
@@ -12,3 +12,12 @@ TEST(FTstrnstr, itShouldReturnFirstOccurrenceOfTheWordInsideHaystack) {
 
 	ASSERT_EQ(haystack + 6, result);
 }
+
+TEST(FTstrnstr, itShouldReturnFirstOccurrenceLookingUpToNBytesOnHaystack) {
+	const char *haystack = "hello world!";
+	const char *search = "hello";
+
+	char *result = ft_strnstr(haystack, search, ft_strlen("hello"));
+
+	ASSERT_EQ(haystack, result);
+}

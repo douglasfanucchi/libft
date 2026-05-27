153bde39ff1df9f4103b15639ca85b4a828b7292
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-27 05:36:23 -0300

    tests: ft_strtrim should not remove any character when set is empty or NULL

--- tests/ft_strtrim_test.cc
+++ tests/ft_strtrim_test.cc
@@ -12,3 +12,18 @@ TEST(FTstrtrim, itShouldRemoveAnyCharacterInSetFromTheBeginingAndFromTheEnd) {
 
 	ASSERT_STREQ("hello world", result);
 }
+
+TEST(FTstrtrim, itShouldNotRemoveAnyCharacterWhenSetIsEmptyOrNull) {
+	const char *str = " hello world-/";
+
+	char *result = ft_strtrim(str, "");
+
+	ASSERT_STREQ(" hello world-/", result);
+	ASSERT_NE(str, result);
+	free(result);
+	
+	result = ft_strtrim(str, NULL);
+	ASSERT_STREQ(" hello world-/", result);
+	ASSERT_NE(str, result);
+	free(result);
+}

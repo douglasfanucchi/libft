06160195c5084022b5dc7756526dabebc09d78e7
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-27 15:46:32 -0300

    tests: ft_strchr should compare char as unsigned char

--- tests/ft_strchr_test.cc
+++ tests/ft_strchr_test.cc
@@ -27,3 +27,11 @@ TEST(FTstrchr, itShouldReturnNullTerminatedByte) {
 
 	ASSERT_EQ(str + 5, result);
 }
+
+TEST(FTstrchr, itShouldCompareCharsAsUnsignedChar) {
+	char str[7] = {'h', 'e', 'l', 'l', 'o', '!', 0};
+
+	char *result = ft_strchr(str, 'h' + 256);
+
+	ASSERT_EQ(str, result);
+}

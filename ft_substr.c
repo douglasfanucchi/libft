1dfb4fb8257ecf76421480c970fd9655ec7506d3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-28 12:46:44 -0300

    refact: ft_substr only allocates necessary memory and return empty string when index is invalid

--- ft_substr.c
+++ ft_substr.c
@@ -5,10 +5,12 @@ char	*ft_substr(const char *str, unsigned int i, size_t len)
 	size_t	size;
 	char	*result;
 
-	len = sizeof(char) * (len + 1);
 	size = ft_strlen(str);
+	if (len > size - i)
+		len = size - i;
+	len = sizeof(char) * (len + 1);
 	if (i >= size)
-		return ((void *)0);
+		return (ft_strdup(""));
 	result = malloc(len);
 	ft_strlcpy(result, str + i, len);
 	return (result);

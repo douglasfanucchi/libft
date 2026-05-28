4ad16ec159607c92a6cf06209a471817e0104d02
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-28 10:55:00 -0300

    refact: changes invalid space check on ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,10 +4,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
 
-	if (nmemb * size > 2147483647)
+	if (nmemb && size && (nmemb > 2147483647 / size || size > 2147483647 / nmemb))
 		return ((void *)0);
 	result = malloc(nmemb * size);
 	if (result)
-		ft_bzero(result, size);
+		ft_bzero(result, nmemb * size);
 	return (result);
 }

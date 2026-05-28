1e8037a2b0aa0322d879924f0b55e5efe7271a34
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-28 10:51:09 -0300

    fix: ft_atoi skips space and parses plus sign

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,14 +1,24 @@
 #include <libft.h>
 
+static char ft_isspace(char c)
+{
+	return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ');
+}
+
 int	ft_atoi(const char *str)
 {
 	char	is_negative;
 	int		result;
 
 	result = 0;
-	is_negative = str[0] == '-';
-	if (is_negative)
+	is_negative = 0;
+	while (*str && ft_isspace(*str))
 		str++;
+	if (*str == '+' || *str == '-')
+	{
+		is_negative = str[0] == '-';
+		str++;
+	}
 	while (ft_isdigit(*str))
 	{
 		result *= 10;

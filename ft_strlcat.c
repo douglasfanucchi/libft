b20cc2084cf7cd8c7c0cc4469cd1339637e268f2
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-28 12:08:35 -0300

    refact: it considers dsize as dest len when dsize is passed incorectly or dest is not a true C string

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,19 +1,25 @@
 #include <libft.h>
 
+static size_t	ft_min(size_t a, size_t b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
 size_t	ft_strlcat(char *dest, const char *src, size_t dsize)
 {
-	size_t	dest_size;
-	size_t	src_size;
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	free_space;
 
-	dest_size = ft_strlen(dest);
-	src_size = ft_strlen(src);
-	if (dsize < 2)
-		return (src_size);
-	if (dest_size + src_size < dsize)
-		ft_memmove(dest + dest_size, src, src_size + 1);
-	else {
-		ft_memmove(dest + dest_size, src, dsize - 1 - dest_size);
-		dest[dsize - 1] = 0;
-	}
-	return (dest_size + src_size);
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dest_len > dsize)
+		dest_len = dsize;
+	if (!dsize || dest_len == dsize)
+		return(dest_len + src_len);
+	free_space = dsize - dest_len - 1;
+	ft_memmove(dest + dest_len, src, ft_min(src_len, free_space));
+	return (dest_len + src_len);
 }

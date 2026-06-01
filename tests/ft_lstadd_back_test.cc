1701a75862feb631b5ecafb27c51395ba67f82b9
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-01 17:44:27 -0300

    tests: ft_listadd_back should add an element to the end of a non empty list

--- tests/ft_lstadd_back_test.cc
+++ tests/ft_lstadd_back_test.cc
@@ -12,3 +12,19 @@ TEST(FTlistaddback, itShouldAddAnElementToTheEndOfAnEmptyList) {
 	ASSERT_EQ(head, node);
 	free(node);
 }
+
+TEST(FTlistaddback, itShouldAddAnElementToTheEndOfANonEmptyList) {
+	int n1 = 42, n2 = 43;
+	t_list *head = ft_lstnew(&n1);
+
+	ft_lstadd_back(&head, ft_lstnew(&n2));
+
+	ASSERT_EQ(&n1, head->content);
+	ASSERT_EQ(&n2, head->next->content);
+	while (head)
+	{
+		t_list *tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}

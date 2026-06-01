498b0e7647b0ce9205eb1abe1a174e11a74ee866
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-01 16:08:22 -0300

    tests: ft_lstsize should count amount of nodes in a list

--- tests/ft_lstsize_test.cc
+++ tests/ft_lstsize_test.cc
@@ -11,3 +11,14 @@ TEST(FTlstsize, itShouldReturnZeroOnAnEmptyList) {
 
 	ASSERT_EQ(0, result);
 }
+
+TEST(FTlstsize, itShouldReturnAmountOfNodesInAList) {
+	t_list *node = NULL;
+	ft_lstadd_front(&node, ft_lstnew(NULL));
+	ft_lstadd_front(&node, ft_lstnew(NULL));
+	ft_lstadd_front(&node, ft_lstnew(NULL));
+
+	int result = ft_lstsize(node);
+
+	ASSERT_EQ(3, result);
+}

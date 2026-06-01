050f263e867c544976cbb4037cef84355c14ea88
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-01 16:23:17 -0300

    tests: ft_lstlast should return last element of a list

--- tests/ft_lstlast_test.cc
+++ tests/ft_lstlast_test.cc
@@ -11,3 +11,22 @@ TEST(FTlstlast, itShouldReturnNULLWhenListIsEmpty) {
 
 	ASSERT_EQ(NULL, result);
 }
+
+TEST(FTlstlast, itShouldReturnLastElementOfAList)
+{
+	t_list *head = NULL, *node = ft_lstnew((void *)42);
+	ft_lstadd_front(&head, node);
+	ft_lstadd_front(&head, ft_lstnew((void *)1));
+	ft_lstadd_front(&head, ft_lstnew((void *)2));
+	ft_lstadd_front(&head, ft_lstnew((void *)3));
+
+	t_list *result = ft_lstlast(head);
+
+	ASSERT_EQ(node, result);
+	while (head)
+	{
+		t_list *tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}

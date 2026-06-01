930e7e7fa27e9cd5698e1c5c469a919c02354c48
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-01 18:30:41 -0300

    feat: it implements ft_lstiter

--- libft.h
+++ libft.h
@@ -51,5 +51,6 @@ t_list	*ft_lstlast(t_list *head);
 void	ft_lstadd_back(t_list **head, t_list *_new);
 void	ft_lstdelone(t_list *lst, void (*f)(void *));
 void	ft_lstclear(t_list **lst, void (*f)(void *));
+void	ft_lstiter(t_list *node, void (*f)(void *));
 
 #endif
